fa43802d4b22d2d91dde9166ae1eab14615693f2
David Duranty <dadaduranty@gmail.com>
2024-07-04 10:57:21 +0200

    Angular 16: Component New patient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NewPatientComponent } from './new-patient/new-patient.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { OldPatientComponent } from './old-patient/old-patient.component';
+import { NewPatientComponent } from './new-patient/new-patient.component';
 
 @NgModule({
   declarations: [
